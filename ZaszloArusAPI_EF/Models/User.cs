de5973cd758130abcc42d0ca8669fd1904311fdf
Dömötör Gyula <grattlesnake@gmail.com>
2023-01-07 21:32:37 +0100

    Zaszloarus App

---
+++ ZaszloArusAPI_EF/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ZaszloArusAPI_EF.Models
+{
+    public partial class User
+    {
+        public User()
+        {
+            Sales = new HashSet<Sale>();
+        }
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Lvl { get; set; }
+        public bool Active { get; set; }
+
+        public virtual ICollection<Sale> Sales { get; set; }
+    }
+}
